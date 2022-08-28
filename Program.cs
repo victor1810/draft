4d509ef968d1885e9995c8826dea1281473c5089
Виктор Парахин <victor1810@vip-pc.localdomain>
2022-08-28 22:44:15 +0300

    The first problem has been solved

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу на C # Sharp, чтобы отобразить куб числа до заданного целого числа.
+
+int num = 1;
+
+while (num < 6)
+{
+    int cube = (num * num * num);
+    Console.Write($"{cube}, ");
+    num++;
+}
