f8e6c7ec88c44db696076bdb3b70f7376dc68448
Виктор Парахин <victor1810@vip-pc.localdomain>
2022-09-02 21:47:48 +0300

    premarge

--- Program.cs
+++ Program.cs
@@ -1,11 +1,28 @@
 ﻿// Напишите программу на C # Sharp для отображения таблицы умножения заданного целого числа.
 
-Console.Write("Введите число для расчёта: ");
-int num = int.Parse(Console.ReadLine());
-int count = 1;
+// Console.Write("Введите число для расчёта: ");
+// int num = int.Parse(Console.ReadLine()!);
+// int count = 1;
 
-while (count < 11)
-{
-    Console.WriteLine($"{num} x {count} = {num * count}");
-    count++;
-}
+// while (count < 11)
+// {
+//     Console.WriteLine($"{num} x {count} = {num * count}");
+//     count++;
+// }
+
+
+
+/* Напишите программу, которая принимает на вход три числа
+и выдаёт максимальное из этих чисел. */
+
+// int a = new Random().Next(100);
+// int b = new Random().Next(100);
+// int c = new Random().Next(100);
+
+// int max = a;
+
+// if (b > max)
+//     max = b;
+// if (c > max)
+//     max = c;
+// Console.Write($"a = {a}, b = {b}, c = {c}. max ->  {max}");
