a55f0dc28a044b74a12d4d2a22643fc1abbafa26
victor1810 <victor181078@gmail.com>
2022-09-03 18:57:50 +0300

    Postponed until tomorrow

--- Program.cs
+++ Program.cs
@@ -26,42 +26,30 @@
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3                         */
 
-// int[] CreatedArray(int size, int min = 0, int max = 100)
+// Console.Write($"Введи несколько чисел -> ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[] massiveNumbers = new int[m];
+
+// void InputNumbers(int m)
 // {
-//     int[] array = new int[size];
-//     for (int i = 0; i < size; i++)
-//         array[i] = new Random().Next(min, max);
-//     return array;
+//     for (int i = 0; i < m; i++)
+//     {
+//         Console.Write($"Введи {i + 1} число: ");
+//         massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+//     }
 // }
 
-// foreach (var _el in CreatedArray(8))
-//     Console.Write($"{_el} ");
+// int Comparison(int[] massiveNumbers)
+// {
+//     int count = 0;
+//     for (int i = 0; i < massiveNumbers.Length; i++)
+//     {
+//         if (massiveNumbers[i] > 0) count += 1;
+//     }
+//     return count;
+// }
 
+// InputNumbers(m);
 
-Console.Write($"Введи число М(количество чисел): ");
-int m = Convert.ToInt32(Console.ReadLine());
-int[] massiveNumbers = new int[m];
+// Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
 
-void InputNumbers(int m)
-{
-    for (int i = 0; i < m; i++)
-    {
-        Console.Write($"Введи {i + 1} число: ");
-        massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
-    }
-}
-
-
-int Comparison(int[] massiveNumbers)
-{
-    int count = 0;
-    for (int i = 0; i < massiveNumbers.Length; i++)
-    {
-        if (massiveNumbers[i] > 0) count += 1;
-    }
-    return count;
-}
-
-InputNumbers(m);
-
-Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
