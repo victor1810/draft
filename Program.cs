032130bab139a1c030472cdc988ec330be87eb7d
victor1810 <victor181078@gmail.com>
2022-09-09 20:46:19 +0300

    add trenings file

--- Program.cs
+++ Program.cs
@@ -1,63 +1,22 @@
-﻿/* Задача 56: Задайте прямоугольный двумерный массив. Напишите программу,
-которая будет находить строку с наименьшей суммой элементов.          */
+﻿/* Известна стоимость монитора, системного блока, клавиатуры и мыши.
+Сколько будут стоить 3 компьютера из этих элементов? N компьютеров? */
 
-int m = InputNumbers("");
-int n = InputNumbers("");
+Console.WriteLine();
+int monitor = InputNumbers("Введите цену на монитор: ");
+int system = InputNumbers("Введите цену на системный блок: ");
+int keyboard = InputNumbers("Введите цену на клавиатуру: ");
+int mouse = InputNumbers("Введите цену на компьютерную мышь: ");
+int n = InputNumbers("Введите количество компьютеров: ");
+Console.WriteLine();
 
-int[,] array = new int[m, n];
-CreateArray(array);
-PrintArray(array);
+int set = monitor + system + keyboard + mouse;
 
-int minSumLine = 0;
-int minSum = SumLineElements(array, 0);
-for (int i = 1; i < array.GetLength(0); i++)
-{
-    int tempMinSum = SumLineElements(array, i);
-    if (minSum > tempMinSum)
-    {
-        minSum = tempMinSum;
-        minSumLine = i;
-    }
-}
-
-Console.WriteLine($"\n{minSumLine + 1} - строкa с наименьшей суммой ({minSum})");
-
-void CreateArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(10);
-        }
-    }
-}
-
-int SumLineElements(int[,] array, int i)
-{
-    int sumLine = array[i, 0];
-    for (int j = 1; j < array.GetLength(1); j++)
-    {
-        sumLine += array[i, j];
-    }
-    return sumLine;
-}
+Console.WriteLine($"Цена за 1 компьютер = {set} денег");
+Console.WriteLine($"Цена за {n} компьютеров = {n * set} денег\n");
 
 int InputNumbers(string input)
 {
     Console.Write(input);
-    int output = new Random().Next(3, 10);
+    int output = Convert.ToInt32(Console.ReadLine());
     return output;
-}
-
-void PrintArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
 }
