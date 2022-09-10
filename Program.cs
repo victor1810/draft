1dc7a12123d026bcbc1f29884b3a2dfb2ce67d94
victor1810 <victor181078@gmail.com>
2022-09-10 06:59:59 +0300

    fff

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
 /* Известна стоимость монитора, системного блока, клавиатуры и мыши.
 Сколько будут стоить 3 компьютера из этих элементов? N компьютеров? */
 
-Console.WriteLine();
-int monitor = InputNumbers("Введите цену на монитор: ");
-int system = InputNumbers("Введите цену на системный блок: ");
-int keyboard = InputNumbers("Введите цену на клавиатуру: ");
-int mouse = InputNumbers("Введите цену на компьютерную мышь: ");
-int n = InputNumbers("Введите количество компьютеров: ");
+int monitor = InputNumbers();
+int system = InputNumbers();
+int keyboard = InputNumbers();
+int mouse = InputNumbers();
+int n = InputNumbers();
 Console.WriteLine();
 
 int set = monitor + system + keyboard + mouse;
@@ -14,9 +13,9 @@ int set = monitor + system + keyboard + mouse;
 Console.WriteLine($"Цена за 1 компьютер = {set} денег");
 Console.WriteLine($"Цена за {n} компьютеров = {n * set} денег\n");
 
-int InputNumbers(string input)
+int InputNumbers(string, )
 {
-    Console.Write(input);
-    int output = Convert.ToInt32(Console.ReadLine());
+    Console.Write();
+    int output = new Random().Next(100, 100000);
     return output;
 }
